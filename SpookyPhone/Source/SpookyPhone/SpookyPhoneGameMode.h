04aa60ad39327ca43e257a166078248ea7f29b21
Matt DeWolfe <matthewdewolfe@gmail.com>
2015-01-26 15:00:15 -0500

    Initial commit of blank project

---
+++ SpookyPhone/Source/SpookyPhone/SpookyPhoneGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SpookyPhoneGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPOOKYPHONE_API ASpookyPhoneGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
